af288f9572d49d2f4956eb6a05924d75b3ec96bf
Nathan Herzing <nherzing@gmail.com>
2014-12-09 22:30:35 -0500

    older version of core.async because...

--- project.clj
+++ project.clj
@@ -5,19 +5,17 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "1.0.3"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2234"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/clojurescript "0.0-2411"]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
                  [reagent "0.4.3"]]
   :cljsbuild
-  {:builds [{:source-paths ["src"]
-             :id "dev"
-             :compiler {:preamble ["reagent/react.js"]
-                        :output-dir "out"
-                        :output-to "main.js"
-                        :pretty-print true}}
-            {:source-paths ["src"]
-             :id "prod"
-             :compiler {:preamble ["reagent/react.js"]
-                        :output-to "prod.js"
-                        :pretty-print false
-                        :optimizations :advanced}}]})
+  {:builds {:dev {:source-paths ["src"]
+                  :compiler {:preamble ["reagent/react.js"]
+                             :output-dir "out"
+                             :output-to "main.js"
+                             :pretty-print true}}
+            :prod {:source-paths ["src"]
+                   :compiler {:preamble ^:replace ["reagent/react.min.js"]
+                              :output-to "prod.js"
+                              :pretty-print false
+                              :optimizations :advanced}}}})
